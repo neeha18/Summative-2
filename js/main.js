a11b146bdad0520f2a102b05718e198dd1d583fb
neeha18 <62521093+neeha18@users.noreply.github.com>
2020-04-03 11:34:43 +1300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+console.log("HELLO WORLD!");
+
+
+function toggleMenu() {
+
+    //SET X TO BE OUR #menu ELEMENT
+    var x = document.getElementById("menu");
+    //SET Y TO BE OUR #navButton ELEMENT
+    var y = document.getElementById("navButton");
+
+    //TOGGLE (EITHER ADD OR REMOVE) THE .displayMenu CLASS TO #menu ELEMENT
+    x.classList.toggle("displayMenu");
+    //TOGGLE THE .fa-toggle-on CLASS FROM FONTAWESOME to the #navButton ELEMENT
+    y.classList.toggle("fa-toggle-on")
+
+}
